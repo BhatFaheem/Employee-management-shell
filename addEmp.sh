75df1d6514095baa1a2897098f5dc4bed845035d
Faheem Ul Islam <fuislam@Faheems-MacBook-Pro.local>
2025-02-06 22:33:42 +0530

    Setting up the main.sh file

--- addEmp.sh
+++ addEmp.sh
@@ -1,3 +1,12 @@
 Employee_Id="$1"
 Employee_File="$2"
 
+read -p "Enter the name of Employee : " emp_name
+while [[ ! "$emp_name" =~ ^[A-Za-z]{1 , 25}$ ]];
+    do
+        echo "Invalid Name . Please try a valid name : "
+        read emp_name
+done
+
+echo "Employee added successfully :) "
+echo "$Employee_Id:$emp_name" >> "$2"

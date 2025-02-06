8c0898e1995997b533ebdd064b22a9c8b8173456
Faheem Ul Islam <fuislam@Faheems-MacBook-Pro.local>
2025-02-06 21:50:50 +0530

    Setting up the main.sh file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Employee-management-shell
+# First we will create a main script file from where our program will start it's execution
+# Then we will create differnt files to create our program
+# All the Employee details will be seperated by a colon(:)
+# All the emplyee details will be validated before saving 
---
+++ main.sh
@@ -0,0 +1,41 @@
+Employee_File="employee.dat"
+Employee_Id=1000
+while true;
+    do
+    echo "=============================="
+    echo " Employee Management System"
+    echo "=============================="
+    
+    echo "Welcome :) What do you want to do --> "
+    echo "1) Add a new Employee "
+    echo "2) Modify existing Employee "
+    echo "3) Delete a Employee "
+    echo "4) List all Employee's "
+    echo "5) Quit "
+    echo "=============================="
+    
+    read option
+    case $option in
+        1)
+            echo "Adding Employee..."
+            ./addEmp.sh "$Employee_Id" "$Employee_File"
+            ;;
+        2)
+           echo "Modifying User..."
+           ./modifyEmp.sh "$Employee_Id" "$Employee_File"
+           ;;
+        3)
+            echo "Deleting User..."
+           ./delEmp.sh "$Employee_Id" "$Employee_File"
+           ;;
+        4)
+            echo "Listing all User's..."
+           ./listEmp.sh "$Employee_File"
+           ;;
+        5)
+            echo "Quiting..."
+            exit 0
+            ;;
+    esac
+done
+
